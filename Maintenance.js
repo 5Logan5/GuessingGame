895176354c4a40e0b5e25fccde2d60715796f193
5Logan5 <45473968+5Logan5@users.noreply.github.com>
2019-05-17 21:35:24 -0400

    V.12 Maintenance Break

--- Maintenance.js
+++ Maintenance.js
@@ -1,1 +1,1 @@
-document.write("I'm currently under maintenance break reverting back to V.11.5! I should be done at 9:15 PM Est")
+document.write("I'm currently under maintenance break trying to get V.12 delivered nice and smoothly. Trust me it's worth the wait! I should be done at 9:50 PM Est")
