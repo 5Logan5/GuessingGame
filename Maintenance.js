9da2c0a39e4337bfc02e00ca8da6b916912625ed
5Logan5 <45473968+5Logan5@users.noreply.github.com>
2019-05-29 21:13:32 -0400

    v.13
    Added an existing game mode where you get to choose how much tries you have

--- Maintenance.js
+++ Maintenance.js
@@ -1,1 +1,1 @@
-document.write("I'm currently under maintenance break working hard to get v.12.7 delivered to you nice and smoothly! I should be done at 4:30 PM Est")
+document.write("I'm currently under maintenance break working hard to get v.13 delivered to you nice and smoothly! I should be done at 9:20 PM Est")
